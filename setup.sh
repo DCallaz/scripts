02712b7de1a7d03c755153657ca6d4320e30bcb6
Dylan Sean Callaghan <21831599@sun.ac.za>
2023-08-15 14:42:56 +0200

    Separated out bash local and global settings.
    [Details]
    Moved things in .bashrc that are shared among set-ups to the .bash_global,
    which can be distributed with this repository. Local settings such as
    directory names are now contained in the normal .bashrc, which sources
    the .bash_global to load global settings. Also adjusted the setup script
    to deal with this change.
    
    [Bugs]
    Changed setup script to not copy the README.
    
    [File's changed]
    
    	renamed:    .bashrc -> .bash_global
    	modified:   setup.sh

--- setup.sh
+++ setup.sh
@@ -4,13 +4,26 @@ SCRIPTS=$(pwd)
 for file in $FILES
 do
   if [[ "$file" != ".." ]] && [[ "$file" != "." ]] && [[ "$file" != ".git" ]] &&
-    [[ "$file" != "setup.sh" ]]; then
+    [[ "$file" != "setup.sh" ]] && [ "$file" != "README.md" ]; then
     #echo $file
     cd ~/
     ln -sf "$SCRIPTS/$file"
     #echo "$SCRIPTS/$file"
   fi
 done
+# Set up bash_global link
+if [ -f ~/.bashrc ]; then
+  if [ "$(head -n 1 ~/.bashrc)" == "#!/bin/bash" ]; then
+    # Add below shebang
+    sed -i '2s;^;# Include global settings\nsource ~/.bash_global\n\n;' ~/.bashrc
+  else
+    # Add as first line
+    sed -i '1s;^;# Include global settings\nsource ~/.bash_global\n\n;' ~/.bashrc
+  fi
+else
+    # Create new file
+    echo "# Include global settings\nsource ~/.bash_global\n" > ~/.bashrc
+fi
 # Set up vim plug and load all plugins
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
