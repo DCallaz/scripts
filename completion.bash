e1266396114195a04298d9a67cbc00caed492668
Dylan Sean Callaghan <21831599@sun.ac.za>
2023-08-15 16:30:09 +0200

    Fixed up command and completion

--- completion.bash
+++ completion.bash
@@ -1,7 +1,7 @@
 #/usr/bin/env bash
 _up () {
   local cur=${COMP_WORDS[COMP_CWORD]}
-  local LS=$(echo $PWD)
+  local LS="$PWD"
   local LSARRAY=${LS//\// }
   COMPREPLY=( $(compgen -W "${LSARRAY}" -- $cur) )
   return 0
