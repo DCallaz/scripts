260a175411891244b0e5bda785d62fe79ac2aa17
Dylan Callaghan <21831599@sun.ac.za>
2025-03-12 10:24:41 +0200

    Updated completion for tmx

--- completion.bash
+++ completion.bash
@@ -9,13 +9,23 @@ _up () {
 complete -F _up up
 _tmx () {
   local cur=${COMP_WORDS[COMP_CWORD]}
-  local TMUX_ACTIVE=$(tmux ls -F#{session_name} 2>&1)
-  if [[ "$TMUX_ACTIVE" == *"no server running"* ]] || [[ "$TMUX_ACTIVE" == *"error connecting to"* ]]; then
-    TMUX_ACTIVE=""
-  fi
-  local TMUX_SAVED=$(cat ~/bin/.tmux.formats | sed 's/::.*//')
-  local TMUX="$TMUX_ACTIVE $TMUX_SAVED"
-  COMPREPLY=( $(compgen -W "${TMUX}" -- $cur) )
+  case $3 in
+    # autocomplete directories
+    -d)
+      compopt -o nospace
+      COMPREPLY=( $(compgen -d -o default -- $cur) )
+      ;;
+    # else autocomplete tmx session names
+     *)
+       local TMUX_ACTIVE=$(tmux ls -F#{session_name} 2>&1)
+       if [[ "$TMUX_ACTIVE" == *"no server running"* ]] || [[ "$TMUX_ACTIVE" == *"error connecting to"* ]]; then
+         TMUX_ACTIVE=""
+       fi
+       local TMUX_SAVED=$(cat ~/bin/.tmux.formats | sed 's/::.*//')
+       local TMUX="$TMUX_ACTIVE $TMUX_SAVED"
+       COMPREPLY=( $(compgen -W "${TMUX}" -- $cur) )
+      ;;
+  esac
   return 0
 }
 complete -F _tmx tmx
